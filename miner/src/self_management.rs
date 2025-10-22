b8c23333934986a5f8f91809e03dba92e66bed39
Tom Bleek <tom@proton.me>
2025-10-22 19:36:37 +0200

    Create new session on update to detach from rust binary

--- miner/src/self_management.rs
+++ miner/src/self_management.rs
@@ -3,6 +3,7 @@ use std::{
     process::{Command, Stdio},
     io::Write,
 };
+use std::os::unix::process::CommandExt;
 
 use crate::{builder::validate_miner_type, error::{Error, Result}, global_config};
 
@@ -26,10 +27,9 @@ pub fn install_self(
         .env("ACCOUNT_SEED", account_seed)
         .spawn()?;
 
-    {
-        let stdin = child.stdin.as_mut().ok_or(Error::custom("Failed to get stdin"))?;
-        stdin.write_all(INSTALLER)?;
-    }
+    child.stdin.take()
+        .ok_or(Error::custom("Failed to get stdin"))?
+        .write_all(INSTALLER)?;
 
     let output = child.wait_with_output()?;
 
@@ -51,56 +51,67 @@ pub fn install_self(
 }
 
 pub fn try_apply_update_if_available() -> Result<()> {
-    // TODO: validate binary before updating with checksum or version
-    // Depending on time taken to update, we might need to add a call to the parachain to set miner to inactive before update and to active after
-
     let current_version = env!("CARGO_PKG_VERSION");
     println!("Trying to update from version {} to latest.", current_version);
-
-    let mut installer_path = std::env::temp_dir();
-    installer_path.push("update.sh");
-    std::fs::write(&installer_path, INSTALLER)?;
-
-    let output = Command::new("bash")
-        .arg(&installer_path)
+    
+    let mut check_cmd = Command::new("bash")
+        .arg("-s")
         .arg("check-update")
         .arg(current_version)
-        .output()?;
-
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+    
+    check_cmd.stdin.as_mut()
+        .ok_or_else(|| Error::from("Failed to open stdin"))?
+        .write_all(INSTALLER)?;
+    
+    let output = check_cmd.wait_with_output()?;
     let stdout = String::from_utf8_lossy(&output.stdout);
     let latest_tag = stdout.trim();
     let stderr = String::from_utf8_lossy(&output.stderr);
-
+    
     if !output.status.success() {
         eprintln!("Update check failed: {stderr}");
         return Ok(());
     }
-
+    
     if stdout.is_empty() {
         println!("No update available.");
         return Ok(());
     }
-
+    
     println!("Update available: {}", latest_tag);
-
+    
     let log_file = std::fs::OpenOptions::new()
         .create(true)
         .append(true)
         .open(&global_config::PATHS.updater_log_path)?;
+    
+    writeln!(&log_file, "\n===== Starting update from {} at {:?} =====", 
+             current_version, chrono::Utc::now())?;
+    
+    unsafe {
+        let mut update_cmd = Command::new("bash")
+            .arg("-s")
+            .arg("update")
+            .arg(current_version)
+            .arg(latest_tag)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::from(log_file.try_clone()?))
+            .stderr(Stdio::from(log_file))
+            .pre_exec(|| {
+                libc::setsid();
+                Ok(())
+            })
+            .spawn()?;
 
-    writeln!(&log_file, "\n===== Starting update from {} at {:?} =====", current_version, chrono::Utc::now())?;
-
-    Command::new("bash")
-        .arg(&installer_path) // read commands from stdin
-        .arg("update")
-        .arg(current_version)
-        .arg(latest_tag)
-        .stdin(Stdio::null())
-        .stdout(Stdio::from(log_file.try_clone()?))
-        .stderr(Stdio::from(log_file))
-        .spawn()?;
-
+        update_cmd.stdin.take()
+            .ok_or_else(|| Error::from("Failed to open stdin"))?
+            .write_all(INSTALLER)?;
+    }
+    
     println!("Updater launched, check {} for logs", global_config::PATHS.updater_log_path.display());
-
     std::process::exit(0);
 }
