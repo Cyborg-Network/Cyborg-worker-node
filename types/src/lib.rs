b1daceabecebc2fddda3f1142bb3405a4c0b85b5
Tom Bleek <tom.bleek@proton.me>
2025-11-24 17:33:55 +0100

    Add types

---
+++ types/src/lib.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use subxt::utils::AccountId32;
+
+pub mod substrate_interface;
+
+pub type MinerIdVec = Vec<u8>;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MinerIdentity {
+    pub miner_owner: AccountId32,
+    pub miner_id: MinerIdVec,
+    pub miner_type: substrate_interface::api::runtime_types::cyborg_primitives::miner::MinerType,
+}
+
+#[derive(Debug, Clone)]
+pub struct CurrentTask {
+    pub id: u64,
+    pub task_type: substrate_interface::api::runtime_types::cyborg_primitives::task::TaskKind<u32>,
+    pub container_name: String,
+    pub task_owner: AccountId32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TaskOwner {
+    pub task_owner: String,
+}
+
+pub struct MinerConfig {
+    pub domain: String,
+    pub latitude: i32,
+    pub longitude: i32,
+    pub ram: u64,
+    pub storage: u64,
+    pub cpu: u16,
+}
